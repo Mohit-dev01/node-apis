36584371633285d566eabb8772a377bce940c506
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-05-27 23:28:53 +0530

    setup routes and controllers

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const products_routes = require("./routes/products");
 
 app.get("/", (req, res) => {
   console.log("request", req), res.send("Hi, I am live");
 });
+
+app.use("/api/products", products_routes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
